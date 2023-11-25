5105405429fc0434912364a83de168acf64ebb16
Pedro Figueiredo <pedrohfm2012@yahoo.com.br>
2023-11-25 00:41:19 -0300

    concertando erros de português

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const Page = () => {
       <Header />
       <section className='bg-white flex justify-center items-center flex-col pb-16 px-5 text-center'>
         <h1 className='font-semibold text-desconto text-2xl sm:text-2xl lg:text-3xl pt-10 pb-3'>Destaques</h1>
-        <span className='text-freteFree lg:text-xl sm:text-base pb-10 font-medium'>Frete grátis e chinelo de bronde é aqui, aproveite por tempo limitado</span>
+        <span className='text-freteFree lg:text-xl sm:text-base pb-10 font-medium'>Frete grátis e chinelo de brinde é aqui, aproveite por tempo limitado</span>
         <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 justify-center items-center text-desconto px-5'>
           {tennisList.map((item, index)=> (
             <PhotoItem key={index} photo={item} />
